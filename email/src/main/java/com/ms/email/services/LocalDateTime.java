ae1b8edc59a6fddec51e4c2a6f6f2d1b6dc3d4ea
Samuel Ningum Pedroso <samuel-mm@hotmail.com.br>
2024-10-01 06:20:37 -0300

    Entrega de projeto

---
+++ email/src/main/java/com/ms/email/services/LocalDateTime.java
@@ -0,0 +1,10 @@
+package com.ms.email.services;
+
+public class LocalDateTime {
+
+    public static java.time.LocalDateTime now() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'now'");
+    }
+
+}
